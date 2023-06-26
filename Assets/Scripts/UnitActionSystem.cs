1ac6d30b7241ac6b8f07cb3be74e60bf46e71e18
Joaquin Heredia <joaquinheredia93@gmail.com>
2023-06-26 16:21:00 -0400

    Adds unit selection visuals as requested in #2 (#3)
    Co-authored-by: Joaquin Heredia <joaquin.heredia@rappi.com>

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -1,18 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitActionSystem : MonoBehaviour
 {
-    private static UnitActionSystem instance;
+    public static UnitActionSystem Instance { get; private set; }
+
+    public event EventHandler OnSelectedUnitChanged;
 
     [SerializeField] private Unit selectedUnit;
     [SerializeField] private LayerMask unitsLayerMask;
 
     private void Awake()
     {
-        instance = this;
+        if (Instance != null)
+        {
+            Debug.LogError($"Multiple instances of UnitActionSystem present {transform} - {Instance}");
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
     }
+    /// <summary>
+    /// On click, handle unit selection or movement
+    /// On key up, check for movement speed modifications
+    /// </summary>
     private void Update()
     {
         if (Input.GetMouseButtonUp(0))
@@ -20,6 +33,33 @@ public class UnitActionSystem : MonoBehaviour
             if (TryHandleUnitSelection()) return;
             selectedUnit.Move(MouseWorld.GetPosition());
         }
+        HandleMovementSpeedInteractions();
+    }
+
+    /// <summary>
+    /// Attempt to select an unit in the units layer mask of the scene. Cast a ray from the mouse position to the game world and in
+    /// case of a hit on a component retrieve that Unit component and set it as a the selected unit for this instance.
+    /// </summary>
+    /// <returns>True if an Unit component was hit; False otherwise</returns>
+    private bool TryHandleUnitSelection()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, unitsLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent<Unit>(out Unit unit))
+            {
+                SetSelectedUnit(unit);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Check whether the arrow up, arrow down or r key were pressed and change movement speed values accordingly
+    /// </summary>
+    private void HandleMovementSpeedInteractions()
+    {
         //Slow player (10%)
         if (Input.GetKeyUp(KeyCode.DownArrow))
         {
@@ -38,19 +78,22 @@ public class UnitActionSystem : MonoBehaviour
             selectedUnit.SetMovementSpeed(selectedUnit.baseMovementSpeed);
         }
     }
-
-    private bool TryHandleUnitSelection()
+    /// <summary>
+    /// Change this Instance <c>Unit</c> and emit an event notifying of the change
+    /// </summary>
+    /// <param name="unit"></param>
+    private void SetSelectedUnit(Unit unit)
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, instance.unitsLayerMask))
-        {
-            if (raycastHit.transform.TryGetComponent<Unit>(out Unit unit))
-            {
-                instance.selectedUnit = unit;
-                return true;
-            }
-        }
-        return false;
+        selectedUnit = unit;
+        OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty);
     }
 
+    /// <summary>
+    /// Return the selected unit for this instance
+    /// </summary>
+    /// <returns>The selected unit on this instance</returns>
+    public Unit GetSelectedUnit()
+    {
+        return selectedUnit;
+    }
 }
---
+++ Assets/Scripts/UnitSelectedVisual.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSelectedVisual : MonoBehaviour
+{
+    [SerializeField] private Unit unit;
+
+    // Texture of the selector
+    private MeshRenderer meshRenderer;
+
+    // Get the mesh renderer and assign it
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+
+    private void Start()
+    {
+        UnitActionSystem.Instance.OnSelectedUnitChanged += UnitActionSystem_OnSelectedUnitChanged;
+        UpdateVisual();
+    }
+
+    /// <summary>
+    /// Listens to unit selection change on the class <c>UnitActionSystem</c>
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="empty"></param>
+    private void UnitActionSystem_OnSelectedUnitChanged(object sender, EventArgs empty)
+    {
+        UpdateVisual();
+    }
+
+    /// <summary>
+    /// UpdateVisual enables or disables the mesh renderer of the unit's selected visual object
+    /// </summary>
+    private void UpdateVisual()
+    {
+        meshRenderer.enabled = UnitActionSystem.Instance.GetSelectedUnit() == unit;
+    }
+}
