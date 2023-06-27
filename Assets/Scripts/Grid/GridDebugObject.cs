15a4e93e2823b0d3e8aefdcea717e79154345c8d
Joaquin Heredia <joaquin.heredia@rappi.com>
2023-06-26 23:39:59 -0400

    Implements fully functional camera and grid system, pending checks to avoid any overflows or unexpected behaviours

---
+++ Assets/Scripts/Grid/GridDebugObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GridDebugObject : MonoBehaviour
+{
+    private GridObject gridObject;
+    [SerializeField] private TextMeshPro textMeshPro;
+    public void SetGridObject(GridObject gridObject)
+    {
+        this.gridObject = gridObject;
+    }
+
+    private void Update()
+    {
+        textMeshPro.SetText(gridObject.ToString());
+    }
+}
---
+++ Assets/Scripts/LevelGrid.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGrid : MonoBehaviour
+{
+
+    public static LevelGrid Instance { get; private set; }
+
+    [SerializeField] private Transform gridDebugObjectPrefab;
+    private GridSystem gridSystem;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError($"Multiple instances of LevelGrid present {transform} - {Instance}");
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+        gridSystem = new GridSystem(10, 10, 2f);
+        gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddUnitAtGridPosition(GridPosition gridPosition, Unit unit)
+    {
+        GridObject gridObject = gridSystem.GetGridObject(gridPosition);
+        gridObject.AddUnit(unit);
+    }
+
+    public List<Unit> GetUnitListAtGridPosition(GridPosition gridPosition)
+    {
+        GridObject gridObject = gridSystem.GetGridObject(gridPosition);
+        if (gridObject != null)
+        {
+            return gridObject.GetUnitList();
+        }
+        return null;
+    }
+
+    public void RemoveUnitAtGridPosition(GridPosition gridPosition, Unit unit)
+    {
+        GridObject gridObject = gridSystem.GetGridObject(gridPosition);
+        if (gridObject != null)
+        {
+            gridObject.RemoveUnit(unit);
+        }
+    }
+
+    public void UnitMovedGridPosition(Unit unit, GridPosition fromPosition, GridPosition targetPosition)
+    {
+        RemoveUnitAtGridPosition(fromPosition, unit);
+        AddUnitAtGridPosition(targetPosition, unit);
+    }
+    public GridPosition GetGridPosition(Vector3 WorldPosition) => gridSystem.GetGridPosition(WorldPosition);
+}
